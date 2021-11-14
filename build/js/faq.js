cf934fb5f8fe30cb2d25d38412325658680b18d2
mazax1ct <vova@giveandget.ru>
2021-11-14 17:52:30 +0300

    pages

--- build/js/faq.js
+++ build/js/faq.js
@@ -3,3 +3,30 @@ $(document).on('click', '.faq__accordion-toggler', function () {
   $(this).closest('.faq__accordion').find('.faq__accordion-body').slideToggle();
   return false;
 });
+
+$(document).on('focus', '.search__input', function () {
+  $('.search__dropdown').fadeIn();
+  document.addEventListener('click', closeSearchDrop);
+  return false;
+});
+
+function closeSearchDrop(evt) {
+  if (!$('.search').is(evt.target) && $('.search').has(evt.target).length === 0) {
+    document.removeEventListener('click', closeSearchDrop);
+    $('.search__dropdown').fadeOut();
+	}
+}
+
+$(document).on('click', '.search__dropdown-option', function () {
+  $('.search__input').val($(this).text());
+  $('.search__button').prop('disabled', '');
+  return false;
+});
+
+$(document).on('input', '.search__input', function () {
+	if($(this).val().length > 0) {
+		$('.search__button').prop('disabled', '');
+	}else{
+		$('.search__button').prop('disabled', 'disabled');
+	}
+});
